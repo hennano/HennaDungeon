f8ec8f3fdf982c0a289bdd3c241c4f8f105ae324
hennano <pittyama37@gmail.com>
2020-11-17 21:57:27 +0900

    EnemyEntityにAI参照用のattack()実装

--- src/main/java/net/hennabatch/hennadungeon/entity/EnemyEntity.java
+++ src/main/java/net/hennabatch/hennadungeon/entity/EnemyEntity.java
@@ -1,9 +1,11 @@
 package net.hennabatch.hennadungeon.entity;
 
 import net.hennabatch.hennadungeon.dungeon.Dungeon;
+import net.hennabatch.hennadungeon.item.WeaponItem;
 import net.hennabatch.hennadungeon.vec.EnumDirection;
 import net.hennabatch.hennadungeon.vec.Vec2d;
 
+import java.lang.ref.WeakReference;
 import java.util.Random;
 
 public abstract class EnemyEntity extends AiEntity implements IAttackable{
@@ -12,6 +14,7 @@ public abstract class EnemyEntity extends AiEntity implements IAttackable{
         super(pos, dungeon);
     }
 
+    //リアクション用
     public void attack(EnumDirection direction) {
         Random rand = new Random();
         boolean isCompleted = getDungeon().getEntities().stream()
@@ -21,6 +24,19 @@ public abstract class EnemyEntity extends AiEntity implements IAttackable{
                 .anyMatch(x -> x.onAttacked(this, getStatus().getATK(getEquipmentWeapon(), getEquipmentArmor()), getEquipmentWeapon().isMagic(), getEquipmentWeapon().isMelee(), getEquipmentWeapon().giveEffectsForAttacker(rand.nextDouble())));
         if(isCompleted){
             getEquipmentWeapon().giveEffectsForAttacker(rand.nextDouble()).forEach(x -> getStatus().addEffect(x));
+        }
+    }
+
+    //AI用
+    public void attack(WeaponItem item, EnumDirection direction){
+        Random rand = new Random();
+        boolean isCompleted = getDungeon().getEntities().stream()
+                .filter(x -> x instanceof BreakableEntity)
+                .filter(x -> item.isInnerRange(this, x, direction))
+                .map(x -> (BreakableEntity)x)
+                .anyMatch(x -> x.onAttacked(this, getStatus().getATK(item, getEquipmentArmor()), getEquipmentWeapon().isMagic(), item.isMelee(), item.giveEffectsForAttacker(rand.nextDouble())));
+        if(isCompleted){
+            item.giveEffectsForAttacker(rand.nextDouble()).forEach(x -> getStatus().addEffect(x));
         }
     }
 }
