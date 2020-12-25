a89904d18aff8a700160a7483e74f5d24eed4bf1
hennano <pittyama37@gmail.com>
2020-12-25 16:41:41 +0900

    KeyConfigの変更が追加になっていたバグの修正

--- src/main/java/net/hennabatch/hennadungeon/config/KeyConfig.java
+++ src/main/java/net/hennabatch/hennadungeon/config/KeyConfig.java
@@ -5,35 +5,35 @@ import java.util.Map;
 
 public class KeyConfig {
 
-    private final Map<Character, EnumKeyInput> keys = new HashMap<>();
+    private final Map<EnumKeyInput, Character> keys = new HashMap<>();
 
     public KeyConfig setDefault(){
-        this.keys.put('w', EnumKeyInput.UP);
-        this.keys.put('s', EnumKeyInput.DOWN);
-        this.keys.put('a', EnumKeyInput.LEFT);
-        this.keys.put('d', EnumKeyInput.RIGHT);
-        this.keys.put('l', EnumKeyInput.SUBMIT);
-        this.keys.put(';', EnumKeyInput.CANCEL);
-        this.keys.put('q', EnumKeyInput.MENU);
-        this.keys.put('e', EnumKeyInput.SKILL);
-        this.keys.put('f', EnumKeyInput.SEEPATH);
+        this.keys.put(EnumKeyInput.UP, 'w');
+        this.keys.put(EnumKeyInput.DOWN, 's');
+        this.keys.put(EnumKeyInput.LEFT, 'a');
+        this.keys.put(EnumKeyInput.RIGHT, 'd');
+        this.keys.put(EnumKeyInput.SUBMIT, 'l');
+        this.keys.put(EnumKeyInput.CANCEL, ';');
+        this.keys.put(EnumKeyInput.MENU, 'q');
+        this.keys.put(EnumKeyInput.SKILL, 'e');
+        this.keys.put(EnumKeyInput.SEEPATH, 'f');
         return this;
     }
 
     public EnumKeyInput getByChar(char ch){
+        return this.keys.entrySet().stream().filter(x -> x.getValue() == ch).map(Map.Entry::getKey).findFirst().orElse(null);
+    }
+
+    public Character getChar(EnumKeyInput key){
         try {
-            return this.keys.get(ch);
+            return this.keys.get(key);
         }catch(Exception e){
             return null;
         }
     }
 
-    public char getChar(EnumKeyInput key){
-        return this.keys.entrySet().stream().filter(x -> x.getValue() == key).map(Map.Entry::getKey).findFirst().orElse(null);
-    }
-
-    public void replaceKey(EnumKeyInput key, char c){
-        keys.remove(getChar(key));
-        keys.put(c, key);
+    public void replaceKey(EnumKeyInput keyInput, char c){
+        keys.remove(keyInput);
+        keys.put(keyInput, c);
     }
 }
