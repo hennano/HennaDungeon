9619d14cb929788baefca51296cbc00869b03c97
hennano <pittyama37@gmail.com>
2020-12-25 18:10:15 +0900

    起動時ログに操作方法を表示するように変更

--- src/main/java/net/hennabatch/hennadungeon/HennaDungeon.java
+++ src/main/java/net/hennabatch/hennadungeon/HennaDungeon.java
@@ -17,6 +17,16 @@ public class HennaDungeon {
 
         //ゲーム初期化
         RootEvent root = new RootEvent();
+        Reference.logger.info(Reference.config.keyConfig().getChar(EnumKeyInput.UP) + ":上");
+        Reference.logger.info(Reference.config.keyConfig().getChar(EnumKeyInput.DOWN) + ":下");
+        Reference.logger.info(Reference.config.keyConfig().getChar(EnumKeyInput.LEFT) + ":左");
+        Reference.logger.info(Reference.config.keyConfig().getChar(EnumKeyInput.RIGHT) + ":右");
+        Reference.logger.info(Reference.config.keyConfig().getChar(EnumKeyInput.SUBMIT) + ":決定");
+        Reference.logger.info(Reference.config.keyConfig().getChar(EnumKeyInput.CANCEL) + ":キャンセル");
+        Reference.logger.info(Reference.config.keyConfig().getChar(EnumKeyInput.MENU) + ":メニューを開く");
+        Reference.logger.info(Reference.config.keyConfig().getChar(EnumKeyInput.SKILL) + ":スキルを使用する");
+        Reference.logger.info(Reference.config.keyConfig().getChar(EnumKeyInput.SEEPATH) + ":帰り道を確認する");
+        Reference.logger.info("詳細はゲーム内メニューのヘルプにて");
         Reference.logger.logScreen().overWrite(root.drawScreen(new Screen(Reference.SCREEN_WIDTH, Reference.SCREEN_HEIGHT))).println();
 
 
